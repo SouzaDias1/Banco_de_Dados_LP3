2bce4cff45a5d70daed4f232cc644ad9f8a28d93
Suzan-Queiroz <Suzanliviaqueiroz@icloud.com>
2022-05-17 17:17:44 -0300

    refactor: add database setup class

---
+++ Database/DatabaseSetup.cs
@@ -0,0 +1,48 @@
+using Microsoft.Data.Sqlite;
+
+namespace Banco_de_Dados_LP3.Database;
+
+class DatabaseSetup
+{
+
+    public DatabaseSetup()
+    {
+        CreatTableComputer();
+        CreatTableLab();
+    }
+
+
+    public void CreatTableComputer()
+    {
+        var connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS Computers(
+                id int not null primary key,
+                ran varchar(100) not null,
+                processor varchar(100) not null
+            );
+        ";
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+
+    public void CreatTableLab()
+    {
+        var connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS Lab(
+                id int not null primary key,
+                ran varchar(100) not null,
+                processor varchar(100) not null
+            );
+        ";
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,7 @@
 ﻿using Microsoft.Data.Sqlite;
+using Banco_de_Dados_LP3.Database;
 
-var connection = new SqliteConnection("Data Source=database.db");
-connection.Open();
-
-var command = connection.CreateCommand();
-command.CommandText = @"
-    CREATE TABLE IF NOT EXISTS Computers(
-        id int not null primary key,
-        ran varchar(100) not null,
-        processor varchar(100) not null
-    );
-";
-command.ExecuteNonQuery();
-connection.Close();
+ new DatabaseSetup();
 
 //Routing
 var modelName = args [0];
@@ -23,10 +12,10 @@ if(modelName == "Computer")
     if(modelAction == "List")
     {
         Console.WriteLine("Computer List");
-        connection = new SqliteConnection("Data Source=database.db");
+        var connection = new SqliteConnection("Data Source=database.db");
         connection.Open();
         
-        command = connection.CreateCommand();
+        var command = connection.CreateCommand();
         command.CommandText = "SELECT * FROM Computers;";
         
         var reader = command.ExecuteReader();
@@ -46,10 +35,10 @@ if(modelName == "Computer")
         var ram = args[3];
         var processor = args[4];
 
-        connection = new SqliteConnection("Data Source=database.db");
+        var connection = new SqliteConnection("Data Source=database.db");
         connection.Open();
         
-        command = connection.CreateCommand();
+        var command = connection.CreateCommand();
         command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor);";
         command.Parameters.AddWithValue("$id", id);
         command.Parameters.AddWithValue("$ram", ram);
